360c72ab9bf809d530a1199a870ce1e8d996509c
YoungHyun Kim <yh97yhyh@naver.com>
2025-03-20 14:43:50 +0900

    day20 player jump and attack

--- 2DSideScrollStudy/Assets/Scripts/Player.cs
+++ 2DSideScrollStudy/Assets/Scripts/Player.cs
@@ -9,6 +9,9 @@ public class Player : MonoBehaviour
     Animator animator;
     Rigidbody2D rigid2D;
     SpriteRenderer sp;
+    public GameObject slash;
+
+    public bool isJump = false;
 
     void Start()
     {
@@ -20,11 +23,18 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        HandleKeyInput();
+        FlipPlayer();
         Move();
+        Jump();
+        Attack();
     }
 
-    private void HandleKeyInput()
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void FlipPlayer()
     {
         direction.x = Input.GetAxisRaw("Horizontal");
 
@@ -48,4 +58,43 @@ public class Player : MonoBehaviour
     {
         transform.position += direction * speed * Time.deltaTime;        
     }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && !animator.GetBool("isJump"))
+        {
+            rigid2D.linearVelocity = Vector2.zero;
+            rigid2D.AddForce(new Vector2(0, jumpUp), ForceMode2D.Impulse);
+            animator.SetBool("isJump", true);
+            //isJump = true;
+        }
+    }
+
+    private void CheckGround()
+    {
+        Debug.DrawRay(rigid2D.position, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(
+            rigid2D.position, Vector3.down, 1, LayerMask.GetMask("Ground"));
+
+        if (rigid2D.linearVelocityY < 0 && rayHit.collider != null && rayHit.distance < 0.7f)
+        {
+            animator.SetBool("isJump", false);
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetTrigger("Attack");
+            //ShowSlash();
+        }
+    }
+
+    private void AttackSlash()
+    {
+        Instantiate(slash, transform.position, Quaternion.identity);
+    }
+
 }
